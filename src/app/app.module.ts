c05fd8a8ae9ba685ec575e012d6b3fa68a8da1c6
Ian C <peronoloreviso@gmail.com>
2017-09-21 17:59:28 -0500

    Update to record and retrieve the api key from localstorage, also display error message when the apikey is invalid

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -24,6 +25,7 @@ import { MyConfs } from './constants';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, ToastController } from 'ionic-angular';
+
+import { FormBuilder, Validators } from '@angular/forms';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +10,66 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  public apiForm : any;
+  public requestApiKey : boolean;
 
+  constructor(
+    public navCtrl : NavController,
+    public _fb : FormBuilder,
+    public storage : Storage,
+    public toastCtrl : ToastController
+    ) {
+    this.storage = storage;
+    this.requestApiKey = true;
+    //ej. 1k43Y6yfQX3mrunU9TRR9hH5swL2IXOfiZwJ0n0n
+    let apiKeyRegex = '[a-zA-Z0-9]{40}';
+    this.apiForm=_fb.group({
+     api: ["", Validators.compose([Validators.pattern(apiKeyRegex),Validators.required])]
+   });
   }
 
+  /**
+  *
+  */
+  presentToast(message) {
+
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: 'top'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
+  }
+
+  /**
+  * storeApiKey will save in localstorage the api key provided for the user
+  */
+  storeApiKey(apiForm){
+    console.log(apiForm)
+    if(apiForm.valid){
+      this.storage.set('api_key',apiForm.value.api);
+    } else {
+      this.presentToast('Api value not valid');
+    }
+  }
+
+  /**
+  * checkApiKey will check if we already have an api key
+  */
+  checkApiKey(){
+    this.storage.get('api_key').then((apiKey) => {
+      if(apiKey){
+        this.requestApiKey = false;
+      }
+    });
+  }
+
+  ionViewWillEnter(){
+    this.checkApiKey();
+  }
 }
